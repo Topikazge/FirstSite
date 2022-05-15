c3fcc8e8ae880cb618b25d9c6b8ad5d8ccc6d47d
Macroflai <49238165+Topikazge@users.noreply.github.com>
2022-05-15 18:00:40 +0300

    оке

--- Scripts/ListScript.js
+++ Scripts/ListScript.js
@@ -1,9 +1,12 @@
 function flipflop(id) {
+    closeFolder = "/Image/CloseFolder.svg"
+    closeFolder = "/Image/OpenFolder.svg"
     // id -- идентификатор элемента, который надо скрыть или показать
     // получаем этот элемент
     element = document.getElementById(id);
+    document.getElementById("pyc").setAttribute('src',openFolder);
     // если таковой в документе существует
     if (element)
-        // меняем ему видимость
         element.style.display = element.style.display == "none" ? "" : "none";
+
 }

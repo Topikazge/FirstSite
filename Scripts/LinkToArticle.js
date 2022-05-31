8c1919085f34a37ed109d61d78083f19fa4ff796
Macroflai <49238165+Topikazge@users.noreply.github.com>
2022-05-31 10:38:14 +0300

    переход из блога в справочник

---
+++ Scripts/LinkToArticle.js
@@ -0,0 +1,22 @@
+function GoToArticle(article,nameDoc){
+    document.location.href = article +".html";
+    if (document.readyState == 'loading') {
+    document.addEventListener("DOMContentLoaded", jjjj(nameDoc));
+    }
+}
+
+function jjjj(nameDoc) {
+    if(document.)
+    var xhr = new XMLHttpRequest();
+    alert('1 - ')
+    xhr.open('GET', nameDoc+'.html', true);
+    alert('2 - ')
+    xhr.onreadystatechange = function () {
+        if (this.readyState !== 4) return;
+        if (this.status !== 200) return; // or whatever error handling you want
+        document.getElementById('content').innerHTML = this.responseText;
+    };
+    alert('3 - ')
+    xhr.send();
+    alert('4- ')
+}
